874008b9980b866213d17433b00d27e4a94a2ee9
Rio John C. Ducusin, Junior <rcducusin4@up.edu.ph>
2024-04-07 23:28:21 +0800

    Edit: data_downloader.py - edit workspace and citation

--- Windows-PC/data_downloader.py
+++ Windows-PC/data_downloader.py
@@ -7,20 +7,20 @@ If you use this dataset in a research paper, please cite it using the following 
     eggplant-and-weeds-detection_dataset,
     title = { Eggplant and Weeds Detection Dataset },
     type = { Open Source Dataset },
-    author = { UPLB Ducusin SP },
-    howpublished = { /url{ https://universe.roboflow.com/uplb-ducusin-sp/eggplant-and-weeds-detection } },
-    url = { https://universe.roboflow.com/uplb-ducusin-sp/eggplant-and-weeds-detection },
+    author = { Rio John Ducusin Junior },
+    howpublished = { \\url{ https://universe.roboflow.com/rio-john-ducusin-junior/eggplant-and-weeds-detection } },
+    url = { https://universe.roboflow.com/rio-john-ducusin-junior/eggplant-and-weeds-detection },
     journal = { Roboflow Universe },
     publisher = { Roboflow },
     year = { 2024 },
     month = { apr },
-    note = { visited on 2024-04-05 },
+    note = { visited on 2024-04-07 },
 }
 '''
 
 from roboflow import Roboflow
 
 rf = Roboflow(api_key="610ndPHwIUHkaPqpdvbj")
-project = rf.workspace("uplbducusinsp-us63c").project("eggplant-and-weeds-detection")
+project = rf.workspace("rio-john-ducusin-junior").project("eggplant-and-weeds-detection")
 version = project.version(10)
 dataset = version.download("yolov8")
